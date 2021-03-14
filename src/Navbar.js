c4bf4cd11956406642264c856a80c2fdecf708f8
Viktor <vhelfrih@gmail.com>
2021-03-14 18:38:37 +0100

    site name changed

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   // const { items, setItems } = useContext(ItemContext);
   return (
     <div className="navbar" >
-      <h1>Basement Inventory</h1>
+      <h1>Household Inventory</h1>
       <div className="links">
         <a href="/">Home</a>
         <a href="/out" style={{color: 'red', fontWeight: 'bold'}} >Out of Stock</a>
