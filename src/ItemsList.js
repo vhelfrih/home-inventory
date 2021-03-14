a1b510e046dc02f1bd556ae3c4f53d59a3b07ecd
Viktor <vhelfrih@gmail.com>
2021-03-14 17:24:09 +0100

    unused vars fixed

--- src/ItemsList.js
+++ src/ItemsList.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import Item from './Item';
 import { ItemContext } from './ItemContext';
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,8 @@
-import React, { useContext } from 'react';
-import { ItemContext } from './ItemContext';
+// import React, { useContext } from 'react';
+// import { ItemContext } from './ItemContext';
 
 const Navbar = () => {
-  const { items, setItems } = useContext(ItemContext);
+  // const { items, setItems } = useContext(ItemContext);
   return (
     <div className="navbar" >
       <h1>Basement Inventory</h1>
